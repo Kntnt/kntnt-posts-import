75f422d71c23c95e019e1179abbb5988f5a1c494
Thomas Barregren <thomas@kntnt.se>
2020-10-26 23:30:45 +0100

    Rewrote _log().

--- classes/Logger.php
+++ classes/Logger.php
@@ -22,36 +22,35 @@ trait Logger {
         return $out;
     }
 
+    // If `$message` isn't a string, its value is printed. If `$message` is
+    // a string, it is written with each occurrence of '%s' replaced with
+    // the value of the corresponding additional argument converted to string.
+    // Any percent sign that should be written must be escaped with another
+    // percent sign, that is `%%`. The message is prefixed with [$context]
+    // followed by […] where … is the qualified name of the function calling.
     public static function info( $message = '', ...$args ) {
-        return self::_log( 'INFO', $message, ...$args );
+        return self::_log( 'INFO', $message, $args );
     }
 
+    // If `$message` isn't a string, its value is printed. If `$message` is
+    // a string, it is written with each occurrence of '%s' replaced with
+    // the value of the corresponding additional argument converted to string.
+    // Any percent sign that should be written must be escaped with another
+    // percent sign, that is `%%`. The message is prefixed with [$context]
+    // followed by […] where … is the qualified name of the function calling.
     public static function error( $message = '', ...$args ) {
-        return self::_log( 'ERROR', $message, ...$args );
+        return self::_log( 'ERROR', $message, $args );
     }
 
+    // If `$message` isn't a string, its value is printed. If `$message` is
+    // a string, it is written with each occurrence of '%s' replaced with
+    // the value of the corresponding additional argument converted to string.
+    // Any percent sign that should be written must be escaped with another
+    // percent sign, that is `%%`. The message is prefixed with [$context]
+    // followed by […] where … is the qualified name of the function calling.
     public static function debug( $message = '', ...$args ) {
         if ( self::is_debugging() ) {
-            return self::_log( 'DEBUG', $message, ...$args );
-        }
-    }
-
-    public static function log( $context, $message = '', ...$args ) {
-        if ( in_array( strtoupper( $context ), [ 'INFO', 'ERROR' ] ) || self::is_debugging() ) {
-            return self::_log( $context, $message, ...$args );
-        }
-    }
-
-    public static final function trace( $message = '', ...$args ) {
-        if ( ! is_string( $message ) ) {
-            $args = [ $message ];
-            $message = '%s';
-        }
-        $message = sprintf( $message, ...array_map( [ Plugin::class, 'stringify' ], $args ) );
-        $trace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS );
-        self::_echo( '[TRACE][#1]', $trace[1], $message );
-        for ( $i = 2; $i < count( $trace ); ++ $i ) {
-            self::_echo( "[TRACE][#{$i}]", $trace[ $i ] );
+            return self::_log( 'DEBUG', $message, $args );
         }
     }
 
@@ -61,26 +60,51 @@ trait Logger {
     // Any percent sign that should be written must be escaped with another
     // percent sign, that is `%%`. The message is prefixed with [$context]
     // followed by […] where … is the qualified name of the function calling.
-    protected static function _log( $context, $message, ...$args ) {
+    public static function log( $context, $message = '', ...$args ) {
+        if ( in_array( strtoupper( $context ), [ 'INFO', 'ERROR' ] ) || self::is_debugging() ) {
+            return self::_log( $context, $message, $args );
+        }
+    }
+
+    // If `$message` isn't a string, its value is printed. If `$message` is
+    // a string, it is written with each occurrence of '%s' replaced with
+    // the value of the corresponding additional argument converted to string.
+    // Any percent sign that should be written must be escaped with another
+    // percent sign, that is `%%`. The message is prefixed with [$context]
+    // followed by […] where … is the qualified name of the function calling.
+    public static final function trace( $message = '', ...$args ) {
+        self::_log( 'TRACE', $message, $args, false );
+    }
+
+    private static function _log( $context, $message, $args, $single = true ) {
+        $trace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, $single ? 3 : 0 );
         if ( ! is_string( $message ) ) {
             $args = [ $message ];
             $message = '%s';
         }
         $message = sprintf( $message, ...array_map( [ Plugin::class, 'stringify' ], $args ) );
-        $trace = debug_backtrace( DEBUG_BACKTRACE_IGNORE_ARGS, 3 );
-        self::_echo( "[$context]", $trace[1], $message );
+        self::_log_echo( $trace, $context, 1, $single, $message );
+        if ( ! $single ) {
+            for ( $n = 2; $n < count( $trace ) - 1; ++ $n ) {
+                self::_log_echo( $trace, $context, $n, $single, $message );
+            }
+        }
         return [
             'context' => $context,
             'message' => $message,
         ];
     }
 
-    private static function _echo( $prefix, $step, $message = '' ) {
-        $caller = $step['function'];
-        if ( isset( $step['class'] ) ) {
-            $caller = $step['class'] . $step['type'] . $caller;
+    private static function _log_echo( $steps, $context, $n, $single, $message = '' ) {
+        $caller = $steps[ $n + 1 ]['function'];
+        if ( isset( $steps[ $n + 1 ]['class'] ) ) {
+            $caller = $steps[ $n + 1 ]['class'] . $steps[ $n + 1 ]['type'] . $caller;
         }
-        error_log( "{$prefix}[$caller]" . ( $message ? " $message" : '' ) );
+        $out = "[$context]" . ( $single ? '' : "[#$n]" ) . "[$caller]";
+        if ( 1 == $n ) {
+            $out = "$out $message";
+        }
+        error_log( $out );
     }
 
 }
