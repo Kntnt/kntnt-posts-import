aa1057f5f32db7482e734521613dcb08d7460b72
Thomas Barregren <thomas@kntnt.se>
2020-11-11 01:01:29 +0100

    Updated local example to match Kntnt Author plugon.

--- kntnt-posts-import.php
+++ kntnt-posts-import.php
@@ -8,7 +8,7 @@
  * Plugin URI:        https://www.kntnt.com/
  * GitHub Plugin URI: https://github.com/Kntnt/kntnt-post-import
  * Description:       Provides a tool to import posts with images, attachments, author, terms and metadata exported with Kntnt Posts Export.
- * Version:           0.3.4
+ * Version:           0.3.5
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
  * License:           GPL-3.0+
